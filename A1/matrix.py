e66e3b2cc735032bab0778ff9368a117d5ee2149
tiff-wang <wangtiffany@gmail.com>
2017-10-03 16:34:55 -0400

    ishly working cholesky decomposition

---
+++ A1/matrix.py
@@ -0,0 +1,106 @@
+
+from __future__ import division
+import math, copy
+
+# import pdb
+
+class Matrix(object):
+	def __init__(self): 
+		pass
+
+
+	#decomposes the matrix A into a lower matrix such that L*L^T = A 
+	#matrix A altered after the method (replaced by lower matrix L)
+	def choleskiDecomposition(self, matrix):
+		size = len(matrix)
+		for j in range(size): 
+			sum = 0
+			for i in range(j):
+				sum += matrix[j][i] * matrix[j][i]
+			# print matrix[j][j] - sum
+			matrix[j][j] = math.sqrt(matrix[j][j] - sum)
+			sum = 0
+			for i in range(j):
+				matrix[i][j] = 0;
+			for i in range(j+1, size):
+				for k in range(j):
+					sum += matrix[i][k] * matrix[j][k]
+				matrix[i][j] = (matrix[i][j] - sum) / matrix[j][j]
+
+		print ("decomposition of matrix: ")
+		for row in matrix: print(row)
+
+
+	#method executes A*A^T. prints resulting matrix 
+	def decompositionCheck(self, matrix):
+		LLT = []
+		for i in range(len(matrix)):
+			LLT.append([])
+			for j in range(len(matrix)):
+				sum = 0 
+				for k in range(len(matrix)):
+					sum += matrix[i][k] * matrix[j][k]
+				LLT[i].append(sum)
+		for row in LLT: 
+			print (row)
+
+
+	#method solves L * y = b, returns y 
+	def solvingLowerMatrix(self, matrix, vector):
+		result = []
+		size = len(vector)
+		for i in range(size):
+			sum = 0
+			for j in range(i):
+				sum += result[j] * matrix[i][j]
+			result.append((vector[i] - sum) / matrix[i][i])
+		return result
+
+	#method solves L^T * x = y, returns x 
+	def solvingTransposeLowerMatrix(self, matrix, vector):
+		result = []
+		size = len(vector)
+		for i in range(size)[::-1]: 
+			sum = 0
+			for j in range(size)[:i:-1]:
+				sum += result[size-j-1] * matrix[j][i]
+			result.append((vector[i] - sum) / matrix[i][i])
+		return result[::-1]
+
+
+	#method solves A * x = b using choleski decomposition, returns x
+	#method clones input matrix, so the input matrix does not change during the process
+	def choleski(self, matrix, vector):
+		matrix_clone = copy.deepcopy(matrix)
+		self.choleskiDecomposition(matrix_clone)
+		y = self.solvingLowerMatrix(matrix_clone, vector)
+	 	result = self.solvingTransposeLowerMatrix(matrix_clone, y)
+	 	return result
+
+
+	#method checks if matrix * solution = vector
+	def checkMatrixSolution(self, matrix, solution, vector):
+		for i in range(len(matrix)):
+			sum = 0
+			for j in range(len(matrix)):
+				sum += matrix[i][j] * solution[j]
+			if math.fabs(vector[i] - sum) > 0.01:
+				return False
+		return True
+
+	def matrixMultiplication(self, matrix, vector):
+		result = []
+		size = len(matrix)
+		for i in range(size):
+			sum = 0
+			for j in range(size):
+				sum += matrix[i][j] * vector[j]
+			result.append(sum)
+
+		return result
+
+	def vectorComparator(self, vector1, vector2):
+		for i in range(len(vector1)):
+			if math.fabs(vector1[i]-vector2[i]) > 0.01 : return False
+
+		return True
---
+++ A1/testMatrix.py
@@ -0,0 +1,66 @@
+from __future__ import division
+from matrix import Matrix
+import math, copy 
+
+m = Matrix()
+
+# matrix_2x2 = [[9, 24], [24, 73]]
+# matrix_3x3 = [[36, 24, -30], [24, 25, -14], [-30, -14, 30]]
+# matrix_4x4 = [[1, 4, 2, 9], [4, 25, 11, 36], [2, 11, 86, 36], [9, 36, 36, 254]]
+matrix_5x5 = [5,0,0,1,0],[0,5,1,1,3],[0,1,2,0,2],[1,1,0,3,1],[0,3,2,1,8]
+
+# print "\nmatrix 2x2: "
+# for row in matrix_2x2: print (row)
+
+# print "\nmatrix 3x3: "
+# for row in matrix_3x3: print (row)
+
+# print "\nmatrix 4x4: "
+# for row in matrix_4x4: print (row)
+
+# print "\nmatrix 5x5: "
+# for row in matrix_5x5: print (row)
+
+# x2 = [1, 2]
+# x3 = [1, 2, 3]
+# x4 = [1, 2, 3, 4]
+x5 = [1, 2, 3, 4, 5]
+
+
+# b2 = m.matrixMultiplication(matrix_2x2, x2)
+# b3 = m.matrixMultiplication(matrix_3x3, x3)
+# b4 = m.matrixMultiplication(matrix_4x4, x4)
+b5 = m.matrixMultiplication(matrix_5x5, x5)
+# b5 = [1, 2, 3, 4, 5]
+
+# print "\nmatrix 2x2 * x2 = {0}".format(b2)
+# print "\nmatrix 3x3 * x3 = {0}".format(b3)
+# print "\nmatrix 4x4 * x4 = {0}".format(b4)
+# print "\nmatrix 5x5 * x5 = {0}".format(b5)
+
+
+# choleski_sln2 = m.choleski(matrix_2x2, b2)
+# choleski_sln3 = m.choleski(matrix_3x3, b3)
+# choleski_sln4 = m.choleski(matrix_4x4, b4)
+# choleski_sln5 = m.choleski(matrix_5x5, b5)
+# test = m.matrixMultiplication(matrix_5x5, choleski_sln5)
+# print ("A*x = {0}".format(test))
+
+# print "x4: {0}".format(x4)
+# print "choleski_sln4: {0}".format(choleski_sln4)
+# print "choleski_sln5: {0}".format(choleski_sln5)
+
+
+
+# print "choleski solution checked for matrix 2: {0}".format(m.vectorComparator(choleski_sln2, x2))
+# print "choleski solution checked for matrix 2: {0}".format(m.vectorComparator(choleski_sln3, x3))
+# print "choleski solution checked for matrix 2: {0}".format(m.vectorComparator(choleski_sln4, x4))
+# print "choleski solution checked for matrix 2: {0}".format(m.vectorComparator(choleski_sln5, x5))
+
+
+m.choleskiDecomposition(matrix_5x5)
+
+
+
+
+
